7fe963c1b02a239770dcc7fecb9f37c936534887
acberd12 <acberd12@gmail.com>
2026-07-23 02:11:14 +0800

    BB

---
+++ api/index.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+$root = dirname(__DIR__);
+$tmp = '/tmp/laravel';
+
+foreach (['views', 'sessions', 'cache'] as $directory) {
+    $path = $tmp.'/'.$directory;
+
+    if (! is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+
+$serverlessEnvironment = [
+    'APP_CONFIG_CACHE' => $tmp.'/config.php',
+    'APP_EVENTS_CACHE' => $tmp.'/events.php',
+    'APP_PACKAGES_CACHE' => $tmp.'/packages.php',
+    'APP_ROUTES_CACHE' => $tmp.'/routes.php',
+    'APP_SERVICES_CACHE' => $tmp.'/services.php',
+    'VIEW_COMPILED_PATH' => $tmp.'/views',
+    'LOG_CHANNEL' => 'stderr',
+    'CACHE_STORE' => 'array',
+    'SESSION_DRIVER' => 'cookie',
+];
+
+foreach ($serverlessEnvironment as $key => $value) {
+    if (getenv($key) === false) {
+        putenv("{$key}={$value}");
+        $_ENV[$key] = $value;
+        $_SERVER[$key] = $value;
+    }
+}
+
+if (file_exists($maintenance = $root.'/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require $root.'/vendor/autoload.php';
+
+/** @var Illuminate\Foundation\Application $app */
+$app = require_once $root.'/bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
